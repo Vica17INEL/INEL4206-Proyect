e8dddb99a7fd99328ac441bf7560e4b1c9dd31ab
Fernandomaldonado3 <127753147+Fernandomaldonado3@users.noreply.github.com>
2023-05-05 13:28:16 -0400

    Update TrolleyProject.c

--- TrolleyProject.c
+++ TrolleyProject.c
@@ -1,40 +1,42 @@
-#include <WiFi.h>
-
+#include <Wifi.h>
+#include "PubSubClient.h"
 #include <stdio.h>
-
 #include "freertos/FreeRTOS.h"
-
 #include "freertos/task.h"
-
-
-
+#include <driver/gpio.h>
+#include <string.h>
 
 #define MONITORWIFI_PERIOD 5000
-
 #define MYTASK_PERIOD 2000
-
 #define STARTUP_DELAY 3000
+#define LED_GPIO GPIO_NUM_2
+#define ON 1
 
+
+const char *mqttServer = "34.199.250.106";
+const char *topic = "testing";
+const int mqttPort = 1883;
+const char *mqtt_username = "FFF";
+const char *mqtt_password = "0123456789";
 
 QueueHandle_t publish_queue;
-
-
 WiFiClient espClient;
+PubSubClient client(espClient);
 
+void callback (char* topic, byte* payload, unsigned int length) { };
 
 void initWiFi() {
 
-const char* ssid = "Stefani Building"; 
-
-const char* password = "1234567890"; 
-
+const char* ssid = "Fernando";
+const char* password = "Fernando";
 
 WiFi.mode(WIFI_STA);
-
 WiFi.begin(ssid, password);
+ 
+ pinMode(LED_GPIO, GPIO_MODE_OUTPUT);
+ digitalWrite(LED_GPIO, ON);
 
-
-while (WiFi.status() != WL_CONNECTED) { 
+while (WiFi.status() != WL_CONNECTED) {
 
 Serial.print(".");
 
@@ -46,14 +48,31 @@ Serial.println(WiFi.localIP());
 
 }
 
+void connectToBroker()
+{
+client.setServer(mqttServer, mqttPort);
+client.setCallback(callback);
+while (!client.connected())
+{
+String client_id = "esp32-client-";
+client_id += String(WiFi.macAddress());
+Serial.printf("The client is connected\n", client_id.c_str());
+if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+Serial.println("Connected");
+} else {
+Serial.print("Not connected");
+Serial.print(client.state());
+delay(2000);
+}
+}
+client.publish(topic, "Localizing your trolley, please wait");
+client.subscribe(topic);
 
-
+}
 
 void monitor_wifi_task(void *p) {
 
-
 int n;
-
 
 while(1) {
 
@@ -89,6 +108,37 @@ delay(10);
 
 }
 
+if((WiFi.SSID(1) == "Stefani Building")){
+Serial.println("Test");
+client.setServer(mqttServer, mqttPort);
+client.setCallback(callback);
+client.publish(topic, "Your trolley in Stephanie");
+client.subscribe(topic);
+}
+
+else if(WiFi.SSID(1) == "Chardome"){
+Serial.println("Your trolley is in Chardome");
+client.setServer(mqttServer, mqttPort);
+client.setCallback(callback);
+client.publish(topic, "Your trolley is in Chardome");
+client.subscribe(topic);
+}
+
+else if(WiFi.SSID(1) == "RUMNET"){
+Serial.println("Your trolley is in Lucchetti");
+client.setServer(mqttServer, mqttPort);
+client.setCallback(callback);
+client.publish(topic, "Your trolley is in Lucchetti");
+client.subscribe(topic);
+}
+else{
+Serial.println("No location found");
+client.setServer(mqttServer, mqttPort);
+client.setCallback(callback);
+client.publish(topic, "Your trolley was not found");
+client.subscribe(topic);
+}
+
 }
 
 Serial.println("");
@@ -99,16 +149,11 @@ xQueueSend(publish_queue, &n, (TickType_t) 0);
 
 vTaskDelay(MONITORWIFI_PERIOD / portTICK_PERIOD_MS);
 
-
 }
 
 }
-
-
-
 
 void my_task(void *p) {
-
 
 int n;
 
@@ -116,7 +161,7 @@ while (1) {
 
 if ( xQueueReceive(publish_queue, &n, (TickType_t) 0) == pdTRUE ) {
 
-Serial.print("In my_task got the message that we discovered this number of hotspots: "); 
+Serial.print("In my task got the message that we discovered this number of hotspots: ");
 
 Serial.println(n);
 
@@ -126,11 +171,7 @@ vTaskDelay(MYTASK_PERIOD / portTICK_PERIOD_MS);
 
 }
 
-
 }
-
-
-
 
 int main(){
 
@@ -144,10 +185,10 @@ vTaskDelay(STARTUP_DELAY / portTICK_PERIOD_MS);
 
 // Initialize WiFi and Connection to MQTT Broker
 
-Serial.println("Connecting to WiFi");
+Serial.println("Localizing your trolley, please wait");
 
 initWiFi();
-
+connectToBroker();
 
 Serial.println("Starting All Tasks");
 
@@ -155,18 +196,12 @@ Serial.println("Starting All Tasks");
 
 xTaskCreate(&monitor_wifi_task, "monitor_wifi_task", 2048,NULL,5,NULL );
 
-
 // Start Task and a Queue that will receive the events from the wifi monitoring
 
 publish_queue = xQueueCreate(10, sizeof(int));
-
 xTaskCreate(&my_task, "my_task", 2048,NULL,5,NULL );
 
-
 }
-
-
-
 
 void setup() { main(); }
 
